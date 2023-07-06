5732f6df9d850338a9197fb6a2290b2aca3b759f
jamest <james.tinkess@gmail.com>
2023-07-06 16:13:22 -0400

    Initial Commit

---
+++ SmartCookie/SmartCookieApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartCookieApp.swift
+//  SmartCookie
+//
+//  Created by James Tinkess on 2023-07-06.
+//
+
+import SwiftUI
+
+@main
+struct SmartCookieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
