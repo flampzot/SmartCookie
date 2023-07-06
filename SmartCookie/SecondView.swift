65cdcee7cd2f7fb2788ab31e39353aee45e2ef26
jamest <james.tinkess@gmail.com>
2023-07-06 16:52:55 -0400

    Second View Text Changes

---
+++ SmartCookie/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  SmartCookie
+//
+//  Created by James Tinkess on 2023-07-06.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text("Hello, Second View World!")
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
